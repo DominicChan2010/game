db90be43dcd159e68609841f99fe81cde7c93601
dragon-ball6 <dominicchanzy@gmail.com>
2022-01-25 22:01:44 +0800

    first commit!

---
+++ script.js
@@ -0,0 +1,25 @@
+const rooms = document.querySelectorAll("button.switch");
+const inv = document.querySelector("div#inv");
+const content = document.querySelector("div#in");
+
+// room content
+let r_t = [
+  ["bed"],
+  ["toilet", "sink", "bathtub"],
+  ["TV", "armchair", "front door"],
+  ["table", "note", "window"],
+  ["rummage"]
+];
+var look = "";
+var curr_r = 0;
+
+
+for (let i in rooms) {
+  rooms[i].addEventListener("click", function() {
+    curr_r = i;
+    
+    for (let j of r_t[curr_r]) {
+      look = look + `<button id="${j}">${j}</button>`;
+    }
+  })
+}
